1d1d6dbed0ea9e2e3a11979ec0b3174209bc9cfe
samfav <samuel.favrichon@gmail.com>
2015-12-17 17:59:42 +0200

    add ploting and saving function

--- main.m
+++ main.m
@@ -2,13 +2,15 @@
 loader;
 population = reshape(initial_population, 1, population_size, number_of_bits)
 index = 1
-fitness = fitness_evaluation(population(index,:,:), fitness_function)
+fitness = zeros(50,500)
+fitness(:,1) = reshape(fitness_evaluation(population(index,:,:), fitness_function), 50,1)
 while index < maximum_iteration %stop_condition, add test on fitness
     parents = selection(population(index, :,:), number_of_parents)
     new_generation = reproduction(parents, crossover_type, crossover_rate, population_size)
     new_generation_mutated = mutation(new_generation, mutation_rate)
     population(index+1, :, :) = new_generation_mutated
-    fitness(index+1, :) = fitness_evaluation(population(index+1,:,:), fitness_function)
+    fitness(:,index+1) = reshape(fitness_evaluation(population(index+1,:,:), fitness_function), 50, 1)
+
     index = index + 1
 end
 
---
+++ store.m
@@ -0,0 +1,8 @@
+function stre = store( X )
+%UNTÝTLED3 Summary of this function goes here
+%   Detailed explanation goes here
+
+stre = X;
+
+end
+
