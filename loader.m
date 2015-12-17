b878f3b9f6d460f7d65a934c45d61d103f9456e0
samfav <samuel.favrichon@gmail.com>
2015-12-17 11:45:56 +0200

    add main file

--- loader.m
+++ loader.m
@@ -14,9 +14,26 @@ else
     error('No input population')
 end
 
+%constant parameters
+%general
+population_size = config.population_size
+number_of_bits = config.number_of_bits
+fitness_limit = config.fitness_limit
+maximu_iteration = config.maximum_iteration
+%mutation parameter
+mutation_rate = config.mutation_rate
+%crossover parameters
+crossover_rate = config.crossover_rate
+crossover_type = config.crossover_type
+
+
 %load fitness function
 if isempty(config.fitness_function)
     error('error no fitness function defined !');
 else
     fitness_function = str2func(config.fitness_function) 
+end
+
+if initial_population
+    class(initial_population)
 end
