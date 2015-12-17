87f7616f5420d7211d48bea453422819805da282
samfav <samuel.favrichon@gmail.com>
2015-12-17 14:08:07 +0200

    add fitness function

---
+++ fitness_evaluation.m
@@ -0,0 +1,6 @@
+function fitness = fitness_evaluation( pop_in, fitness_function )
+
+    fitness=sum(pop_in, 3);
+    
+end
+
--- main.m
+++ main.m
@@ -1,11 +1,15 @@
 %main programm
 loader;
-population = initial_population
+population = reshape(initial_population, 1, population_size, number_of_bits)
 index = 1
-while test_condition:
-    parents = selection(population, index, number_of_parents)
+fitness = fitness_evaluation(population(index,:,:), fitness_function)
+while index < maximum_iteration %stop_condition, add test on fitness
+    parents = selection(population(index, :,:), number_of_parents)
     new_generation = reproduction(parents, crossover_type, crossover_rate, population_size)
     new_generation_mutated = mutation(new_generation, mutation_rate)
-    population(:,:,2) = new_generation_mutated
-    index = index +1
+    population(index+1, :, :) = new_generation_mutated
+    fitness(index+1, :) = fitness_evaluation(population(index+1,:,:), fitness_function)
+    index = index + 1
 end
+
+display_output(pop)
