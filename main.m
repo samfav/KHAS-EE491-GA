9ab75a86638a250b6507f6690c630e2772f2f2e3
samfav <samuel.favrichon@gmail.com>
2015-12-17 13:04:35 +0200

    add function prototypes

--- main.m
+++ main.m
@@ -1,2 +1,11 @@
 %main programm
 loader;
+population = initial_population
+index = 1
+while test_condition:
+    parents = selection(population, index, number_of_parents)
+    new_generation = reproduction(parents, crossover_type, crossover_rate, population_size)
+    new_generation_mutated = mutation(new_generation, mutation_rate)
+    population(:,:,2) = new_generation_mutated
+    index = index +1
+end
