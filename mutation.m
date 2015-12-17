60967cf2baa43148180d6236677d547c4f2dfe54
julled <asfsaf@afs.de>
2015-12-17 13:39:55 +0200

    mutation added

---
+++ mutation.m
@@ -0,0 +1,13 @@
+function pop_out = mutation( pop_in, prob )
+
+    [x,Number_of_Individuals,Number_of_bits]=size(pop_in);    
+    Count_of_Rnd_Nrs=Number_of_Individuals*Number_of_bits;
+    temp_in = reshape(pop_in,[1,Count_of_Rnd_Nrs]);
+    sizArray = size(temp_in);
+    toChangeIdx = rand(sizArray) < prob;
+    temp_in(toChangeIdx) = 1-temp_in(toChangeIdx);
+    
+    pop_out=reshape(temp_in,[1,Number_of_Individuals,Number_of_bits]);
+    
+end
+
